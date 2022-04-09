57e57fba7dfad026643734c3b5cccf137bbe2d51
vaqxai <vaqxai@gmail.com>
2022-04-09 17:14:11 +0200

    code layout

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ use lettercounter::count_letters;
 use clap::Parser;
 
 
-fn train() {
-    let mut pPoland = Perceptron::new(26, 0.1);
-    let mut pEnglish = Perceptron::new(26, 0.1);
-    let mut pGerman = Perceptron::new(26, 0.1);
+fn train(p_polish: &mut Perceptron, p_english: &mut Perceptron, p_german: &mut Perceptron) {
 
     // TODO: Prepare the data
     // TODO: Add multiple iteration training with different data
@@ -19,44 +16,58 @@ fn train() {
     // TODO: Add a parser for user-input
 
     let mut input_letters = count_letters(&fileread::file_to_str("train/polish.txt"));
-    pPoland.train(&input_letters, 1.0);
-    pEnglish.train(&input_letters, 0.0);
-    pGerman.train(&input_letters, 0.0);
+    p_polish.train(&input_letters, 1.0);
+    p_english.train(&input_letters, 0.0);
+    p_german.train(&input_letters, 0.0);
 
     input_letters = count_letters(&fileread::file_to_str("train/english.txt"));
-    pEnglish.train(&input_letters, 1.0);
-    pPoland.train(&input_letters, 0.0);
-    pGerman.train(&input_letters, 0.0);
+    p_english.train(&input_letters, 1.0);
+    p_polish.train(&input_letters, 0.0);
+    p_german.train(&input_letters, 0.0);
 
     input_letters = count_letters(&fileread::file_to_str("train/german.txt"));
-    pGerman.train(&input_letters, 1.0);
-    pPoland.train(&input_letters, 0.0);
-    pEnglish.train(&input_letters, 0.0);
-
-    let test_data = count_letters(&fileread::file_to_str("test.txt"));
-    let outcome_polish = pPoland.predict(&test_data);
-    let outcome_english = pEnglish.predict(&test_data);
-    let outcome_german = pGerman.predict(&test_data);
-
-    println!("Perceptron thinks this text is polish: {} german {} english {} ", outcome_polish, outcome_german, outcome_english);
+    p_german.train(&input_letters, 1.0);
+    p_polish.train(&input_letters, 0.0);
+    p_english.train(&input_letters, 0.0);
 
 }
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long)]
-    name: String,
+fn test(p_polish: &mut Perceptron, p_english: &mut Perceptron, p_german: &mut Perceptron, filepath: &str){
+    let test_data = count_letters(&fileread::file_to_str(filepath));
+    let outcome_polish = p_polish.predict(&test_data);
+    let outcome_english = p_english.predict(&test_data);
+    let outcome_german = p_german.predict(&test_data);
 
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+    println!("Perceptron thinks this text is polish: {} german {} english {} ", outcome_polish, outcome_german, outcome_english);
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, help = "A string to classify with the net", conflicts_with = "testfile")]
+    text: Option::<String>,
+
+    #[clap(short, long, default_value_t = 0.1)]
+    learning_rate: f64,
+
+    #[clap(short = 'f', long, default_value = "test.txt", help = "Path to a file containing text to be classified")]
+    testfile: String,
+
+    #[clap(short, long, default_value_t = 1, help = "The number of times to train the network")]
+    iterations: u8,
 }
 
 fn main() {
 
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println("Hello {}!", args.name)
+    let mut p_polish: Perceptron = Perceptron::new(26, args.learning_rate);
+    let mut p_english: Perceptron = Perceptron::new(26, args.learning_rate);
+    let mut p_german: Perceptron = Perceptron::new(26, args.learning_rate);
+
+    for i in 0..args.iterations {
+        train(&mut p_polish, &mut p_english, &mut p_german);
+        println!("Perceptrons trained {} times", i+1);
+        println!("Polish perceptron: {}\nEnglish perceptron: {}\nGerman perceptron: {}", p_polish, p_english, p_german);
     }
 }
